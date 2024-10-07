b9a65db6ad4554fb402b57ab21f65278e582a91a
LubHR <liubomyrhruzd89@gmail.com>
2024-10-07 22:27:43 +0300

    frontend 2

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import UserListCreateAPIView, UserMeView
+from .views import TestEmailView, UserListCreateAPIView, UserMeView
 
 urlpatterns = [
     path('', UserListCreateAPIView.as_view(), name='user-list'),
     path('/me', UserMeView.as_view(), name='user-me'),
+    path('/test',TestEmailView.as_view(), name='test-email'),
 ]
